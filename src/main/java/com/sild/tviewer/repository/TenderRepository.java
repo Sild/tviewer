f38fe5992ec009f440cc9e5d45a02a69e3bc4417
Sild <failsild@gmail.com>
2016-04-30 21:10:40 +0300

    start to add tender

---
+++ src/main/java/com/sild/tviewer/repository/TenderRepository.java
@@ -0,0 +1,19 @@
+package com.sild.tviewer.repository;
+
+import com.sild.tviewer.model.Tender;
+
+import java.util.List;
+
+public interface TenderRepository {
+
+    public void add(Tender element);
+
+    public void update(Tender element);
+
+    public Tender get(int id);
+
+    public void delete(int id);
+
+    public List<Tender> getAll();
+
+}
