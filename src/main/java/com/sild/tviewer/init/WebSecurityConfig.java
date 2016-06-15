c30c941c992071b33947f51df0e8682692380694
Sild <failsild@gmail.com>
2016-06-15 22:31:20 +0300

    try to use spring security

---
+++ src/main/java/com/sild/tviewer/init/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.sild.tviewer.init;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+* @author Dmitry Korchagin.
+*/
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .antMatchers("/admin/**").access("hasRole('ADMIN')")
+                .antMatchers("/db/**").access("hasRole('ADMIN') and hasRole('DBA')")
+                .and().formLogin()
+                .and().exceptionHandling().accessDeniedPage("/Access_Denied");
+
+    }
+}
